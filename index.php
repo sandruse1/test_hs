3dccd24de61776fa47d514e9293d9ac69bd8e2bd
sandruse <andrusechko@gmail.com>
2018-01-16 04:35:32 +0200

    bug

--- index.php
+++ index.php
@@ -14,7 +14,6 @@ error_reporting(E_ALL);
 define('ROOT', dirname(__FILE__));
 require_once(ROOT.'/components/Router.php');
 require_once(ROOT.'/components/Db.php');
-require_once (ROOT.'/components/Session.php');
 
 //Визиваєм роутер
 
